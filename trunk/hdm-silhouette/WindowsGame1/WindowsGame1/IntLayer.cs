a3b70968a53a34641afcaa8831293f6def38e597
hannes.pernpeintner@gmx.de <hannes.pernpeintner@gmx.de@a58ce4f1-5a65-0da4-8a50-38a4432aa4c7>
2010-10-12 19:08:10 +0000

    Zwei, drei Klassen erstellt, kommentiert

---
+++ trunk/hdm-silhouette/WindowsGame1/WindowsGame1/IntLayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1
+{
+    interface IntLayer
+    {
+        // Hannes: Das Interface wird von den drei Layern, die dynamisch sind implementiert, damit bestimmte Funktionen
+        // garantiert sind.
+        // Wir müssen uns überlegen, wie ein einzelner Layer das Drawen delegiert.
+    }
+}
---
+++ trunk/hdm-silhouette/WindowsGame1/WindowsGame1/Level.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WindowsGame1
+{
+    class Level
+    {
+        Texture2D backgroundImage;              // Hannes: Jedes Level bekommt ein Matte Painting als Background.
+        System.Collections.ArrayList layers;    // Und außerdem noch ein Array von Layern.
+        System.Collections.ArrayList events;    // Die Events werden aus XML-Dateien ausgelesen.
+
+    }
+}
