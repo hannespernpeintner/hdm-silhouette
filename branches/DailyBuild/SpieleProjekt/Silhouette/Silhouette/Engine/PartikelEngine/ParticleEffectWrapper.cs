62aa479dc3a5273ba7335cd80cceafa44c8cacdc
Sascha.Can@googlemail.com <Sascha.Can@googlemail.com@a58ce4f1-5a65-0da4-8a50-38a4432aa4c7>
2010-10-20 21:57:35 +0000

    komplett überarbeitete Architektur, auf neueste Changes vorbereitet

---
+++ branches/DailyBuild/SpieleProjekt/Silhouette/Silhouette/Engine/PartikelEngine/ParticleEffectWrapper.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+//Sascha: Partikel-Engine Klassen
+using ProjectMercury;
+using ProjectMercury.Emitters;
+using ProjectMercury.Modifiers;
+using ProjectMercury.Renderers;
+
+namespace Silhouette.Engine.PartikelEngine
+{
+    class ParticleEffectWrapper
+    {
+        /* Sascha:
+        Klasse hat den Zweck alle Zusatzinformationen über Partikeleffekte zu speichern, 
+        die für unseren Renderer bzw. unser Levelformat nötig sind
+        */
+
+        //Sascha: Zusatzinformationen
+        private ParticleEffect particleEffect;
+        private Vector2 particlePosition;
+
+        //Sascha: Zugriff auf Zusatzinformationen über Properties
+        public ParticleEffect getEffect
+        {
+            get { return particleEffect; }
+            set { particleEffect = value; }
+        }
+
+        public Vector2 getPosition
+        {
+            get { return particlePosition; }
+        }
+
+        public ParticleEffectWrapper(ParticleEffect p, Vector2 v)
+        {
+            particleEffect = p;
+            particlePosition = v;
+        }
+    }
+}
