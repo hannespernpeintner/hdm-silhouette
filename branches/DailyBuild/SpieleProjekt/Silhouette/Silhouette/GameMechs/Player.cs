d066a37f60c7eb1987f194eee05a07e55df8ab48
hannes.pernpeintner@gmx.de <hannes.pernpeintner@gmx.de@a58ce4f1-5a65-0da4-8a50-38a4432aa4c7>
2010-12-08 15:10:02 +0000

    Tastenkonflikt mit Camerasteuerung behoben, Animationsbug gefixt.

--- branches/DailyBuild/SpieleProjekt/Silhouette/Silhouette/GameMechs/Player.cs
+++ branches/DailyBuild/SpieleProjekt/Silhouette/Silhouette/GameMechs/Player.cs
@@ -20,9 +20,9 @@ using FarseerPhysics.Dynamics.Contacts;
 
 namespace Silhouette.GameMechs
 {
-    class Player : DrawableLevelObject
+    public class Player : DrawableLevelObject
     {
-        
+
         public Vector2 centerPosition;      // Hannes: Rectangles für Kollisionserkennung. Muss noch um Kollisionsgruppe erweitert werden.
         public Fixture charRect;
         public Fixture nRect;
@@ -45,7 +45,7 @@ namespace Silhouette.GameMechs
         private Animation nextAnimation;        // Die nächste Animation, die gespielt wird, sobald die aktuelle abgelaufen is oder unterbrochen wird.
 
         private Animation running_left;             // Hier kommen alle Animationen hin, die Tom hat.
-        private Animation running_right;  
+        private Animation running_right;
         private Animation jumping_left;
         private Animation jumping_right;
         private Animation falling_left;
@@ -89,24 +89,43 @@ namespace Silhouette.GameMechs
         {
 
             // Hier müssen alle Sprites geladen werden.
-            idle_left.Load(6, "Sprites/Player/idle_left_0", 6.0f, true);
-            idle_right.Load(6, "Sprites/Player/idle_right_0", 6.0f, true);
-            idleb_left.Load(6, "Sprites/Player/idleb_left_0", 6.0f, true);
-            idleb_right.Load(6, "Sprites/Player/idleb_right_0", 6.0f, true);
-            idlec_left.Load(6, "Sprites/Player/idlec_left_0", 6.0f, true);
-            idlec_right.Load(6, "Sprites/Player/idlec_right_0", 6.0f, true);
-            jumping_left.Load(7, "Sprites/Player/jump_left_0", 8.0f, true);
-            jumping_right.Load(7, "Sprites/Player/jump_right_0", 8.0f, true);
-            running_left.Load(8, "Sprites/Player/walk_left_0", 6.0f, true);
-            running_right.Load(8, "Sprites/Player/walk_right_0", 6.0f, true);
+            idle_left.Load(6, "Sprites/Player/idle_left_0", 1.0f, true);
+            idle_right.Load(6, "Sprites/Player/idle_right_0", 1.0f, true);
+            idleb_left.Load(6, "Sprites/Player/idleb_left_0", 1.0f, true);
+            idleb_right.Load(6, "Sprites/Player/idleb_right_0", 1.0f, true);
+            idlec_left.Load(6, "Sprites/Player/idlec_left_0", 1.0f, true);
+            idlec_right.Load(6, "Sprites/Player/idlec_right_0", 1.0f, true);
+            jumping_left.Load(7, "Sprites/Player/jump_left_0", 0.75f, true);
+            jumping_right.Load(7, "Sprites/Player/jump_right_0", 0.75f, true);
+            running_left.Load(8, "Sprites/Player/walk_left_0", 1.5f, true);
+            running_right.Load(8, "Sprites/Player/walk_right_0", 1.5f, true);
 
             activeAnimation = idlec_right;
+            activeAnimation.start();
             nextAnimation = idlec_right;
             charRect = FixtureManager.CreateRectangle(150, 120, position, BodyType.Dynamic, 1);
             nRect = FixtureManager.CreateRectangle(120, 20, position, BodyType.Dynamic, 0);
             eRect = FixtureManager.CreateRectangle(20, 90, position, BodyType.Dynamic, 0);
             sRect = FixtureManager.CreateRectangle(120, 20, position, BodyType.Dynamic, 0);
             wRect = FixtureManager.CreateRectangle(20, 90, position, BodyType.Dynamic, 0);
+
+            nRect.IgnoreCollisionWith(charRect);
+            nRect.IgnoreCollisionWith(eRect);
+            nRect.IgnoreCollisionWith(sRect);
+            nRect.IgnoreCollisionWith(sRect);
+            eRect.IgnoreCollisionWith(charRect);
+            eRect.IgnoreCollisionWith(nRect);
+            eRect.IgnoreCollisionWith(sRect);
+            eRect.IgnoreCollisionWith(wRect);
+            sRect.IgnoreCollisionWith(charRect);
+            sRect.IgnoreCollisionWith(nRect);
+            sRect.IgnoreCollisionWith(eRect);
+            sRect.IgnoreCollisionWith(wRect);
+            wRect.IgnoreCollisionWith(charRect);
+            wRect.IgnoreCollisionWith(nRect);
+            wRect.IgnoreCollisionWith(eRect);
+            wRect.IgnoreCollisionWith(sRect);
+
             Joint joint0 = JointFactory.CreateWeldJoint(Level.Physics, charRect.Body, nRect.Body, new Vector2(0, -60 / Level.PixelPerMeter), Vector2.Zero);
             Joint joint1 = JointFactory.CreateWeldJoint(Level.Physics, charRect.Body, eRect.Body, new Vector2(75 / Level.PixelPerMeter, 0), Vector2.Zero);
             Joint joint2 = JointFactory.CreateWeldJoint(Level.Physics, charRect.Body, sRect.Body, new Vector2(0, 60 / Level.PixelPerMeter), Vector2.Zero);
@@ -133,7 +152,7 @@ namespace Silhouette.GameMechs
 
         private void UpdateControls(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.A) && !isFalling && !isJumping)
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) && !isFalling && !isJumping)
             {
                 if (facing == 1)
                 {
@@ -143,16 +162,17 @@ namespace Silhouette.GameMechs
                         choseIdleAnimation();
                     }
                 }
-                
+
                 {
                     isRunning = true;
                     activeAnimation = running_left;
+                    activeAnimation.start();
                     nextAnimation = choseIdleAnimation();
                     charRect.Body.ApplyForce(new Vector2(-20, 0));
                 }
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.D) && !isFalling && !isJumping)
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) && !isFalling && !isJumping)
             {
                 if (facing == 0)
                 {
@@ -162,25 +182,28 @@ namespace Silhouette.GameMechs
                         choseIdleAnimation();
                     }
                 }
-                
+
                 {
                     isRunning = true;
                     activeAnimation = running_right;
+                    activeAnimation.start();
                     nextAnimation = choseIdleAnimation();
                     charRect.Body.ApplyForce(new Vector2(20, 0));
                 }
             }
 
-            if (Keyboard.GetState().IsKeyUp(Keys.D) && oldState.IsKeyDown(Keys.D))
+            if (Keyboard.GetState().IsKeyUp(Keys.Right) && oldState.IsKeyDown(Keys.D))
             {
                 isRunning = false;
                 activeAnimation = choseIdleAnimation();
+                activeAnimation.start();
             }
 
-            if (Keyboard.GetState().IsKeyUp(Keys.A) && oldState.IsKeyDown(Keys.A))
+            if (Keyboard.GetState().IsKeyUp(Keys.Left) && oldState.IsKeyDown(Keys.A))
             {
                 isRunning = false;
                 activeAnimation = choseIdleAnimation();
+                activeAnimation.start();
             }
 
             if (Keyboard.GetState().IsKeyDown(Keys.Space) && oldState.IsKeyUp(Keys.Space) && isIdle)
@@ -189,6 +212,7 @@ namespace Silhouette.GameMechs
                 {
                     activeAnimation = jumping_left;
                     activeAnimation.activeFrameNumber = 0;
+                    activeAnimation.start();
                     isJumping = true;
                     isIdle = false;
                     isFalling = false;
@@ -199,6 +223,7 @@ namespace Silhouette.GameMechs
                 {
                     activeAnimation = jumping_right;
                     activeAnimation.activeFrameNumber = 0;
+                    activeAnimation.start();
                     isJumping = true;
                     isIdle = false;
                     isFalling = false;
@@ -216,7 +241,7 @@ namespace Silhouette.GameMechs
             activeAnimation.Update(gameTime, position);
         }
 
-        public Animation choseIdleAnimation() 
+        public Animation choseIdleAnimation()
         {
             // Methode dient um zufällig zw. Idle-Animationen zu wechseln.
             double random = new Random().NextDouble();
@@ -233,7 +258,7 @@ namespace Silhouette.GameMechs
                 }
                 else return idle_left;
             }
-            else 
+            else
             {
                 if (random < 0.1)
                 {
@@ -251,18 +276,20 @@ namespace Silhouette.GameMechs
         {
             if (nextAnimation == null)
             {
-                nextAnimation = choseIdleAnimation();    
+                nextAnimation = choseIdleAnimation();
             }
 
             if (activeAnimation == null)
             {
                 activeAnimation = choseIdleAnimation();
+                activeAnimation.start();
             }
 
             if (activeAnimation.activeFrameNumber == activeAnimation.amount - 1 && isJumping)
             {
                 activeAnimation = nextAnimation;
                 activeAnimation.activeFrameNumber = 0;
+                activeAnimation.start();
                 nextAnimation = null;
             }
 
@@ -270,6 +297,7 @@ namespace Silhouette.GameMechs
             {
                 activeAnimation = nextAnimation;
                 activeAnimation.activeFrameNumber = 0;
+                activeAnimation.start();
                 nextAnimation = choseIdleAnimation();
             }
         }
@@ -284,6 +312,11 @@ namespace Silhouette.GameMechs
                 isFalling = false;
                 isRunning = false;
                 isJumping = true;
+            }
+
+            if (isIdle)
+            {
+                isRunning = false;
             }
 
             if (isJumping && oldPosition.Y < charRect.Body.Position.Y)
@@ -365,11 +398,11 @@ namespace Silhouette.GameMechs
         {
             activeAnimation.Draw(spriteBatch);
             //Das auskommentierte hier kann als Debugview dienen.
-            /*spriteBatch.DrawString(FontManager.Arial, "Standing: " + isIdle.ToString(), new Vector2(300, 20), Color.Black);
+            spriteBatch.DrawString(FontManager.Arial, "Standing: " + isIdle.ToString(), new Vector2(300, 20), Color.Black);
             spriteBatch.DrawString(FontManager.Arial, "Running: " + isRunning.ToString(), new Vector2(300, 45), Color.Black);
             spriteBatch.DrawString(FontManager.Arial, "Jumping: " + isJumping.ToString(), new Vector2(300, 70), Color.Black);
             spriteBatch.DrawString(FontManager.Arial, "Falling: " + isFalling.ToString(), new Vector2(300, 95), Color.Black);
-            spriteBatch.DrawString(FontManager.Arial, charRect.Body.Position.ToString(), new Vector2(300, 120), Color.Black);*/
+            spriteBatch.DrawString(FontManager.Arial, charRect.Body.Position.ToString(), new Vector2(300, 120), Color.Black);
         }
     }
 }
