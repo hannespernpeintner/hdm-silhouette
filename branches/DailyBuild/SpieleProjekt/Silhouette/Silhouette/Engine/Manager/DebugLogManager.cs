43348295a0b023bf23eddaa8e2f5058121ace390
Julius.Flohr@gmail.com <Julius.Flohr@gmail.com@a58ce4f1-5a65-0da4-8a50-38a4432aa4c7>
2013-03-15 19:54:50 +0000

    added missing file

---
+++ branches/DailyBuild/SpieleProjekt/Silhouette/Silhouette/Engine/Manager/DebugLogManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace Silhouette.Engine.Manager
+{
+    static class DebugLogManager
+    {
+
+        public static void writeToLogFile(String message)
+        {
+            String currentLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            String Directory = System.IO.Path.GetDirectoryName(currentLocation);
+            String LogfilePath = System.IO.Path.Combine(Directory, "error.log");
+
+            File.AppendAllText(LogfilePath, System.DateTime.UtcNow.TimeOfDay + ": " + message + Environment.NewLine);
+        }
+    }
+}
