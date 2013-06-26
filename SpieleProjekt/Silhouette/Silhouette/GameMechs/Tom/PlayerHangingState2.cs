9f7cff75833aa33efb2fc1a17f4aa22d8a4dc629
h.pernpeintner@gmail.com <h.pernpeintner@gmail.com@a58ce4f1-5a65-0da4-8a50-38a4432aa4c7>
2013-06-26 22:30:39 +0000

    New PlayerStates, added a Move class and a MoveStack for scripted moves.

---
+++ SpieleProjekt/Silhouette/Silhouette/GameMechs/Tom/PlayerHangingState2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+using Silhouette.Engine;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Dynamics.Joints;
+using FarseerPhysics.Dynamics.Contacts;
+using FarseerPhysics.Controllers;
+using FarseerPhysics.Collision;
+
+using Silhouette.GameMechs.Move;
+
+namespace Silhouette.GameMechs
+{
+    public class PlayerHangingState2 : PlayerState
+    {
+        private MoveStack moves = new MoveStack();
+
+        public PlayerHangingState2(Tom tom, Animation animationLeft, Animation animationRight) 
+            : base(tom, animationLeft, animationRight)
+        {
+        }
+
+        public override void Update(GameTime gt)
+        {
+            if (moves.Finished())
+            {
+                tom.State = tom.HangingState2;
+            }
+            base.Update(gt);
+            moves.Update(gt);
+        }
+
+        public override void onUnset()
+        {
+            tom.CharFix.Body.BodyType = BodyType.Dynamic;
+        }
+
+        public override void onSet(Tom.FacingState facingState)
+        {
+            base.onSet(facingState);
+            tom.CharFix.Body.BodyType = BodyType.Kinematic;
+            tom.CharFix.Body.LinearVelocity = Vector2.Zero;
+            tom.CharFix.Body.AngularVelocity = 0;
+            animationLeft.activeFrameNumber = 0;
+            animationRight.activeFrameNumber = 0;
+            moves.push(new Move.Move(tom, Vector2.Zero, 1000));
+        }
+
+        public override void handleInput(KeyboardState cbs, KeyboardState oks)
+        {
+
+            if (cbs.IsKeyDown(Keys.Space))
+            {
+                tom.State = tom.JumpState;
+            }
+        }
+
+    }
+}
